0ff5d8842fc06b29b8322b020cb56bdf5b30d3ef
Guillaume DUBOYS DE LAVIGERIE <guillaume.duboysdelavigerie@viacesi.fr>
2022-03-18 15:57:33 +0100

    lien vers image

--- php/views/viewAcceuil.php
+++ php/views/viewAcceuil.php
@@ -8,14 +8,14 @@
     <meta name="author" content="" />
     <title>SuperStage</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets\img\logo\logo.png" />
+    <link rel="icon" type="image/x-icon" href="../../assets\img\logo\logo.png" />
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css\styles.css" rel="stylesheet" />
+    <link href="../../css/styles.css" rel="stylesheet" />
 
 </head>
 
@@ -23,7 +23,7 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div class="container">
-            <a class="navbar-brand" href="#page-top"><img src="assets\img\logo\logo.png" alt="..." /></a>
+            <a class="navbar-brand" href="#page-top"><img src="../../assets\img\logo\logo.png" alt="..." /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
@@ -92,7 +92,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets\img\entreprise\airbus.jpg" alt="..." />
+                            <img class="img-fluid" src="../../assets\img\entreprise\airbus.jpg" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Stage en développement Logiciel</div>
@@ -107,7 +107,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets\img\entreprise\orange.jpg" alt="..." />
+                            <img class="img-fluid" src="../../assets\img\entreprise\orange.jpg" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Stage en développement Web</div>
@@ -122,7 +122,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets\img\entreprise\sopra.png" alt="..." />
+                            <img class="img-fluid" src="../../assets\img\entreprise\sopra.png" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Stage Assistant Cyber-sécurité</div>
@@ -137,7 +137,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets\img\entreprise\ca.jpg" alt="..." />
+                            <img class="img-fluid" src="../../assets\img\entreprise\ca.jpg" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Stage en développement informatique</div>
@@ -152,7 +152,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets\img\entreprise\sncf.jpg" alt="..." />
+                            <img class="img-fluid" src="../../assets\img\entreprise\sncf.jpg" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Stage Optimisation Base de Données</div>
@@ -167,7 +167,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets\img\entreprise\sfr.jpg" alt="..." />
+                            <img class="img-fluid" src="../../assets\img\entreprise\sfr.jpg" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Stage en Analyse de données</div>
@@ -185,7 +185,7 @@
     <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                <div class="close-modal" data-bs-dismiss="modal"><img src="../../assets/img/close-icon.svg" alt="Close modal" /></div>
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-8">
@@ -193,7 +193,7 @@
                                 <!-- Project details-->
                                 <h2 class="text-uppercase">Stage en développement Logiciel</h2>
                                 <p class="item-intro text-muted">Airbus</p>
-                                <img class="img-fluid d-block mx-auto" src="assets\img\entreprise\airbus.jpg" alt="..." />
+                                <img class="img-fluid d-block mx-auto" src="../../assets\img\entreprise\airbus.jpg" alt="..." />
                                 <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate,
                                     maiores repudiandae, nostrum, reiciendis facere nemo!</p>
                                 <ul class="list-inline">
@@ -534,7 +534,7 @@
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
+    <script src="../../js/scripts.js"></script>
 
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
