623ec9f2ffa35f92227208fe88b855869c77fe7e
alexdudud <alex.duguy@viacesi.Fr>
2022-03-19 11:44:00 +0100

    fin  CreateStudent.php

--- php/models/CreateStudent.php
+++ php/models/CreateStudent.php
@@ -5,9 +5,17 @@ class CreateStudent extends Model
 
     public function CreateStudent($nom, $prenom, $mail, $username, $mdp, $location)
     {
-        $sql = " insert into user (first_name, last_name, mail, username, password, id_location) VALUES ('" . $prenom . "','" . $nom . "','" . $mail . "','" . $username . "','" . $mdp . "',( select id_location from location where city_location= '" . $location . "') ";
-        $req = $this->getBdd()->prepare($sql);
-        $req->execute();
+        $sqluser = " insert into user (first_name, last_name, mail, username, password, id_location) VALUES ('" . $prenom . "','" . $nom . "','" . $mail . "','" . $username . "','" . $mdp . "',( select id_location from location where city_location= '" . $location . "') ";
+        $requser = $this->getBdd()->prepare($sqluser);
+        $requser->execute();
 
+        $sqlstudent = "insert into student (id_user) values (( select id_user from user where first_name= '" . $prenom . "' and  last_name = '" . $nom . "') )";
+        $reqstudent = $this->getBdd()->prepare($sqlstudent);
+        $reqstudent->execute();
+        if ($reqstudent->rowCount() == 0) {
+
+
+            ?> <strong class="EtudiantCree"> <?php echo "étudiant créé"; ?> </strong><?php
+        }
     }
 }
