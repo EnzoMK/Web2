932440938c9cfc6b8fb691848660efa11a9085de
enzom <enzom@MSI>
2022-03-26 15:49:45 +0100

    changement icons pour mobile+manifest+injection librairie fontawesome

---
+++ php/templates_c/19e7af6fba4fc1cdcd544be1301bd1daea35033a_0.file.head.tpl.php
@@ -0,0 +1,45 @@
+<?php
+/* Smarty version 4.1.0, created on 2022-03-26 15:34:09
+  from 'C:\xampp\htdocs\Web2\vendors\tpl\head.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.1.0',
+  'unifunc' => 'content_623f24615bf7c4_13421495',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '19e7af6fba4fc1cdcd544be1301bd1daea35033a' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Web2\\vendors\\tpl\\head.tpl',
+      1 => 1648305246,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_623f24615bf7c4_13421495 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>SuperStage</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="../assets/img/logo/logo.png"/>
+    <!-- Font Awesome icons (free version)-->
+    <link rel="stylesheet" href="../vendors/fontawesome/css/all.min.css">
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css"/>
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../css/styles.css" rel="stylesheet"/>
+</head>
+<?php }
+}
